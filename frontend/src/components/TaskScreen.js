c30e954817f362df31cc1512e0cc288a45cf8922
NyfyN <67443078+NyfyN@users.noreply.github.com>
2025-01-27 21:47:32 +0100

    Update TaskScreen.js
    Dodano przyciski do synchronizacji i sprawdzenia danych w kolejce

--- frontend/src/components/TaskScreen.js
+++ frontend/src/components/TaskScreen.js
@@ -1,7 +1,7 @@
-import {Text, TextInput, Button, FlatList, View} from 'react-native';
+import { Text, TextInput, Button, FlatList, View } from 'react-native';
 import screenStyles from "./screenStyles";
 
-const TaskScreen = ({ tasks, newTask, setNewTask, onAddTask, onLogout, onDeleteAll }) => (
+const TaskScreen = ({ tasks, newTask, setNewTask, onAddTask, onLogout, onDeleteAll, synchronizeAllTasks, checkTasks }) => (
   <>
     <Text style={screenStyles.header}>Lista zadań</Text>
     <TextInput
@@ -13,16 +13,18 @@ const TaskScreen = ({ tasks, newTask, setNewTask, onAddTask, onLogout, onDeleteA
     <Button title="Dodaj zadanie" onPress={onAddTask} />
     <Button title="Wyloguj się" onPress={onLogout} />
     <Button title="Usuń wszystkie zadania" onPress={onDeleteAll} />
+    <Button title="Synchronizuj zadania" onPress={synchronizeAllTasks} />
+    <Button title="Sprawdź zadania w kolejce" onPress={checkTasks} />
     {tasks.length === 0 ? (
-        <Text>Brak zadań do wyświetlenia.</Text>
+      <Text>Brak zadań do wyświetlenia.</Text>
     ) : (
-        <View style={screenStyles.list}>
-            <Text>Lista zadań:</Text>
-            <FlatList
-                data = {tasks}
-                renderItem={({item}) => <Text key={item.id}>&middot; {item.task}</Text>}
-            />
-        </View>
+      <View style={screenStyles.list}>
+        <Text>Lista zadań:</Text>
+        <FlatList
+          data={tasks}
+          renderItem={({ item }) => <Text key={item.id}>&middot; {item.task}</Text>}
+        />
+      </View>
     )}
   </>
 );
